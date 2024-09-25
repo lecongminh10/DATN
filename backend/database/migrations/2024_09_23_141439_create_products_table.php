0be451535339f0b6493cf5c9a4aa5041edc113f0
DTM-MinhLC <minh_lc@detomo.co.jp>
2024-09-25 20:28:10 +0700

    update migreate

--- backend/database/migrations/2024_09_23_141439_create_products_table.php
+++ backend/database/migrations/2024_09_23_141439_create_products_table.php
@@ -14,29 +14,35 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->foreignId('category_id')->constrained('categories')->onDelete('cascade')->comment('Khóa ngoại liên kết đến bảng Categories');
+            $table->string('code')->unique()->comment('Mã sản phẩm');
             $table->string('name')->comment('Tên sản phẩm');
-            $table->text('description')->nullable()->comment('Mô tả chi tiết');
+            $table->text('short_description')->nullable()->comment('Mô tả ngắn');
+            $table->longText('content')->comment('Mô tả chi tiết sản phẩm');
             $table->string('sku')->unique()->comment('Mã SKU');
-            $table->decimal('price', 10, 2)->comment('Giá sản phẩm');
-            $table->decimal('discount', 10, 2)->nullable()->comment('Giá giảm');
+            $table->decimal('price_regular', 10, 2)->comment('Giá sản phẩm');
+            $table->decimal('price_sale', 10, 2)->nullable()->comment('Giá giảm');
             $table->integer('stock')->comment('Số lượng tồn kho');
-            $table->decimal('rating', 3, 2)->nullable()->comment('Điểm đánh giá');
+            $table->decimal('rating', 2, 1)->nullable()->comment('Điểm đánh giá');
             $table->string('tags')->nullable()->comment('Các thẻ liên quan đến sản phẩm');
             $table->integer('warranty_period')->nullable()->comment('Thời gian bảo hành (tháng)');
             $table->integer('view')->default(0)->comment('Số lượt xem');
             $table->integer('buycount')->default(0)->comment('Số lượng lượt mua');
             $table->integer('wishlistscount')->default(0)->comment('Số lượng lượt yêu thích');
-            $table->string('images')->comment('Ảnh sản phẩm');
-            $table->string('code')->comment('mã sản phẩm');
             $table->boolean('is_active')->default(true)->comment('Cờ kích hoạt sản phẩm');
-            $table->boolean('is_hotdeal')->default(false)->comment('Trạng thái hot của sản phẩm');
-            $table->boolean('is_homeview')->default(false)->comment('Trạng thái hiển thị ra màn hình chủ');
-            $table->boolean('is_hotview')->default(false)->comment('Trạng thái lượt xem hiển thị màn hình chính');
-
+            $table->boolean('is_hot_deal')->default(false)->comment('Trạng thái hot của sản phẩm');
+            $table->boolean('is_show_home')->default(false)->comment('Trạng thái hiển thị ra màn hình chủ');
+            $table->boolean('is_new')->default(false)->comment('Trạng thái lượt xem hiển thị màn hình chính');
+            $table->boolean('is_good_deal')->default(false)->comment('Trạng thái tốt của sản phẩm');
+            // Các trường liên quan đến SEO
+            $table->string('slug')->unique()->comment('Slug của sản phẩm để sử dụng trong URL');
+            $table->string('meta_title')->nullable()->comment('Tiêu đề SEO của sản phẩm');
+            $table->text('meta_description')->nullable()->comment('Mô tả SEO của sản phẩm');
+        
             $table->softDeletes()->comment('Thời gian xóa mềm');
-            $table->foreignId('deleted_by')->nullable()->constrained('users')->comment('Người thực hiện xóa mềm');
-            $table->timestamps(); 
+            $table->foreignId('deleted_by')->nullable()->constrained('users')->nullOnDelete()->comment('Người thực hiện xóa mềm');
+            $table->timestamps();
         });
+        
     }
 
     /**
