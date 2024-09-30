bd7f6e00588c440abe3a470405c98afc392a0254
ND_Phat <dangphat09092003@gmail.com>
2024-09-30 17:44:25 +0700

    add code users

---
+++ backend/app/Models/PermissionValue.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class PermissionValue extends Model
+{
+    use HasFactory, SoftDeletes;
+
+
+    protected $table = 'permissions_values';
+    protected $dates = ['deleted_at'];
+    protected $fillable = [
+        "permissions_id",
+        "value"
+    ];
+
+
+    public function permission()
+    {
+        return $this->belongsTo(Permission::class , 'permissions_id' , 'id');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'permissions_value_users', 'permission_value_id', 'user_id')
+                    ->withTimestamps();
+    }
+}
