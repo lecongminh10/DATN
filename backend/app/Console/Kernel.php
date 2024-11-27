eb82f629c36ac876f8b9be9d94c090065222ece3
khanhvu1412 <dkv1412@gmail.com>
2024-11-27 22:02:52 +0700

    fix-send-email

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -15,8 +15,8 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         // $schedule->command('email:send-scheduled')->everyMinute();
         
-        // Chạy lệnh tự động gửi email lúc 12h đêm mỗi ngày
-        $schedule->command('email:send-scheduled')->dailyAt('07:00');
+        // Chạy lệnh tự động gửi email lúc 10h đêm mỗi ngày
+        $schedule->command('email:send-scheduled')->dailyAt('22:00');
     }
 
     /**
