612f47c475b18c67e5975ba0fdc68112215e8798
phatndph39954 <dangphat09092003@gmail.com>
2024-12-17 16:28:54 +0700

    tagcontroller

--- backend/app/Http/Controllers/TagController.php
+++ backend/app/Http/Controllers/TagController.php
@@ -12,9 +12,6 @@ use Illuminate\Support\Facades\Auth;
 
 class TagController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     protected $tagService;
 
     public function __construct(TagService $tagService)
@@ -28,18 +25,10 @@ class TagController extends Controller
         $tags = $this->tagService->getAllTags($search, $perPage);
         return view('admin.tags.index', compact('tags'));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('admin.tags.create');
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(TagRequest $request)
     {
         $validatedData = $request->validated();
@@ -67,28 +56,16 @@ class TagController extends Controller
             return redirect()->back()->withErrors(['error' => 'Đã xảy ra lỗi khi thêm carrier: ' . $e->getMessage()]);
         }
     }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
         $tags = $this->tagService->getById($id);
         // dd($tags);
         return view('admin.tags.edit', compact('tags'));
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(TagRequest $request, string $id)
     {
         $validatedData = $request->validated();
@@ -116,10 +93,6 @@ class TagController extends Controller
             return redirect()->back()->withErrors(['error' => 'Đã xảy ra lỗi khi cập nhật tags: ' . $e->getMessage()]);
         }
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroyTag(int $id)
     {
         $data = $this->tagService->getById($id);
@@ -150,29 +123,24 @@ class TagController extends Controller
     public function restore($id)
     {
         try {
-            // Gọi service để khôi phục Tag
             $data = $this->tagService->restore_delete($id);
 
-            // Chuẩn bị nội dung log
             $logDetails = sprintf(
                 'Khôi phục Tag: Tên - %s',
                 $data['name']
             );
 
-            // Ghi lại hoạt động admin
             event(new AdminActivityLogged(
                 auth()->user()->id,
                 'Khôi phục',
                 $logDetails
             ));
 
-            // Lưu thông báo vào session và chuyển hướng
             session()->flash('success', 'Khôi phục thẻ thành công!');
             return redirect()->route('admin.tags.deleted')->with([
                 'success' => 'Khôi phục thành công'
             ]);
         } catch (\Exception $e) {
-            // Lưu thông báo lỗi vào session và chuyển hướng
             session()->flash('error', 'Không thể khôi phục thẻ: ' . $e->getMessage());
             return redirect()->route('admin.tags.deleted');
         }
@@ -196,7 +164,6 @@ class TagController extends Controller
     }
     public function deleteMuitpalt(Request $request)
     {
-        // Xác thực dữ liệu đầu vào
         $request->validate([
             'ids' => 'required|array',
             'ids.*' => 'integer',
@@ -215,7 +182,6 @@ class TagController extends Controller
                             $this->destroyTag($id);
                         }
                     }
-                    // Trả về thông báo thành công cho xóa mềm
                     return response()->json(['message' => 'Thẻ đã được xóa mềm thành công.']);
                     break;
 
@@ -226,16 +192,13 @@ class TagController extends Controller
                             $this->hardDeleteTag($id);
                         }
                     }
-                    // Trả về thông báo thành công cho xóa cứng
                     return response()->json(['message' => 'Thẻ đã được xóa cứng thành công.']);
                     break;
 
                 default:
-                    // Trả về thông báo lỗi nếu hành động không hợp lệ
                     return response()->json(['message' => 'Hành động không hợp lệ.'], 400);
             }
         } else {
-            // Trả về thông báo lỗi nếu không có thẻ nào được chọn
             return response()->json(['message' => 'Không có thẻ nào được cung cấp để xóa.'], 400);
         }
     }
